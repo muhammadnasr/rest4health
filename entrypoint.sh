3fe249ebf8be277710f609e4b6130f47b9750beb
Muhammad Hassan Nasr <165384+muhammadnasr@users.noreply.github.com>
2022-02-10 16:22:19 +0300

    Smoothing docker-compose
    Now docker-compose up will do everything and init/migrate the Postgres DB and wait it to be up.
    Totally using .env  no hardcoded variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
+set -e
 
-#if [ "$DATABASE" = "postgres" ]
-#then
-    echo "Waiting for postgres..."
+echo "Waiting for PostgreSQL..."
 
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
-    done
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$DB_HOST" -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "PostgresSQL is unavailable - sleeping"
+  sleep 1
+done
 
-    echo "PostgreSQL started"
-#fi
+echo "PostgreSQL started"
 
-
-echo "before pythoncreate"
-
-python manage.py flush --no-input
-python manage.py createsuperuser --username=admin --email=admin@admin.com --noinput
+#python manage.py flush --no-input
+echo "Run python migrate"
 python manage.py migrate
 
-echo "after pythoncreate"
-exec "$@"
+echo "Run python createsuperuser"
+python manage.py createsuperuser --username=admin --email=admin@admin.com --noinput
+
+echo "Run python runserver"
+python manage.py runserver 0.0.0.0:8000
+
