b162ed5ab6aa00b64bb860e75f1f5acfb6946884
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-02-13 22:43:51 +0300

    Adding REST page size to .env

--- rest4health/settings.py
+++ rest4health/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': os.environ.get("REST_PAGE_SIZE")
 }
 
 MIDDLEWARE = [
