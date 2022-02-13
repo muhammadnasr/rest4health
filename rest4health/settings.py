2d3e09d8b406c0a764dd43f482807e29d19c6984
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-02-13 23:04:27 +0300

    Implement JWT authentication

--- rest4health/settings.py
+++ rest4health/settings.py
@@ -42,12 +42,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters'
+    'django_filters',
+    'rest_framework_simplejwt'  
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': os.environ.get("REST_PAGE_SIZE")
+    'PAGE_SIZE': os.environ.get("REST_PAGE_SIZE"),
+    'DEFAULT_AUTHENTICATION_CLASSES': ( 
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 MIDDLEWARE = [
--- restapp/urls.py
+++ restapp/urls.py
@@ -4,7 +4,10 @@ from .views import tables_views
 from rest_framework import routers
 from restapp.views import reservation_views
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', tables_views.UserViewSet)
@@ -22,7 +25,10 @@ urlpatterns = [
     path('reservations/available/<int:seats_count>/', reservation_views.ReservationAvailable.as_view()),
     path('reservations/today/', reservation_views.ReservationToday.as_view()),
 
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
